a816a91cfef9068f7e0e7faa7f3dbc72d7838c49
jhchoi <foodudt@gmail.com>
2023-08-28 02:37:26 +0900

    #12 - AuditingFields 추출
    AuditingFields 추출

--- src/main/java/com/example/sampleboard/domain/Article.java
+++ src/main/java/com/example/sampleboard/domain/Article.java
@@ -23,9 +23,8 @@ import java.util.Set;
         @Index(columnList = "createAt"),
         @Index(columnList = "createBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,11 +37,6 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createAt;
-    @CreatedBy @Column(nullable = false, length = 100) private String createBy;
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
 
     protected Article() {
     }
--- src/main/java/com/example/sampleboard/domain/ArticleComment.java
+++ src/main/java/com/example/sampleboard/domain/ArticleComment.java
@@ -20,20 +20,14 @@ import java.util.Objects;
         @Index(columnList = "createAt"),
         @Index(columnList = "createBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Setter @ManyToOne(optional = false) private Article article;
     @Setter @Column(nullable = false, length = 500) private String content;
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createAt;
-    @CreatedBy @Column(nullable = false, length = 100) private String createBy;
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
 
     protected ArticleComment() {
     }
