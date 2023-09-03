3063d33f57f3d9f7c5ca1cd1fadbec543246c8b2
jhchoi <foodudt@gmail.com>
2023-09-04 03:43:08 +0900

    #1 - Querydsl 이용 api 검색 옵션 추가
    'QuerydslPredicateExecutor', 'QuerydslBinderCustomizer'

--- src/main/java/com/example/sampleboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/sampleboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,29 @@
 package com.example.sampleboard.repository;
 
 import com.example.sampleboard.domain.ArticleComment;
+import com.example.sampleboard.domain.QArticle;
+import com.example.sampleboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createBy, root.createAt);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+    }
 }
