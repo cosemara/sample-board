b55a3d3235e55328f38006cc8dd38760ee1faae9
jhchoi <foodudt@gmail.com>
2023-11-06 02:11:11 +0900

    Search
    Search

--- src/main/java/com/example/sampleboard/controller/ArticleController.java
+++ src/main/java/com/example/sampleboard/controller/ArticleController.java
@@ -38,6 +38,7 @@ public class ArticleController {
 
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumber);
+        map.addAttribute("searchTypes", SearchType.values());
 
         return "articles/index";
     }
