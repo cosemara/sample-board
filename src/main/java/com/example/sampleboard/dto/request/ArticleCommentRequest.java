d78f0ff7a8e50c10418ef7037a40f90409176d81
jhchoi <foodudt@gmail.com>
2023-11-20 03:08:02 +0900

    #33 - 댓글 저장 삭제를 위한 컨트롤러와 서비스 코드 작성

---
+++ src/main/java/com/example/sampleboard/dto/request/ArticleCommentRequest.java
@@ -0,0 +1,24 @@
+package com.example.sampleboard.dto.request;
+
+import com.example.sampleboard.dto.ArticleCommentDto;
+import com.example.sampleboard.dto.UserAccountDto;
+
+public record ArticleCommentRequest(
+        Long articleId,
+        Long parentCommentId,
+        String content
+) {
+
+    public static ArticleCommentRequest of(Long articleId, String content) {
+        return ArticleCommentRequest.of(articleId, content);
+    }
+
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+        return ArticleCommentDto.of(
+                articleId,
+                userAccountDto,
+                content
+        );
+    }
+
+}
--- src/test/java/com/example/sampleboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/example/sampleboard/service/ArticleCommentServiceTest.java
@@ -45,25 +45,16 @@ class ArticleCommentServiceTest {
     void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments() {
         // Given
         Long articleId = 1L;
-        ArticleComment expectedParentComment = createArticleComment(1L, "parent content");
-        ArticleComment expectedChildComment = createArticleComment(2L, "child content");
-        expectedChildComment.setParentCommentId(expectedParentComment.getId());
-        given(articleCommentRepository.findByArticle_Id(articleId)).willReturn(List.of(
-                expectedParentComment,
-                expectedChildComment
-        ));
+        ArticleComment expected = createArticleComment("content");
+        given(articleCommentRepository.findByArticle_Id(articleId)).willReturn(List.of(expected));
 
         // When
         List<ArticleCommentDto> actual = sut.searchArticleComments(articleId);
 
         // Then
-        assertThat(actual).hasSize(2);
         assertThat(actual)
-                .extracting("id", "articleId", "parentCommentId", "content")
-                .containsExactlyInAnyOrder(
-                        tuple(1L, 1L, null, "parent content"),
-                        tuple(2L, 1L, 1L, "child content")
-                );
+                .hasSize(1)
+                .first().hasFieldOrPropertyWithValue("content", expected.getContent());
         then(articleCommentRepository).should().findByArticle_Id(articleId);
     }
 
@@ -82,7 +73,6 @@ class ArticleCommentServiceTest {
         // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
         then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
-        then(articleCommentRepository).should(never()).getReferenceById(anyLong());
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
@@ -102,26 +92,38 @@ class ArticleCommentServiceTest {
         then(articleCommentRepository).shouldHaveNoInteractions();
     }
 
-    @DisplayName("부모 댓글 ID와 댓글 정보를 입력하면, 대댓글을 저장한다.")
+    @DisplayName("댓글 정보를 입력하면, 댓글을 수정한다.")
     @Test
-    void givenParentCommentIdAndArticleCommentInfo_whenSaving_thenSavesChildComment() {
+    void givenArticleCommentInfo_whenUpdatingArticleComment_thenUpdatesArticleComment() {
         // Given
-        Long parentCommentId = 1L;
-        ArticleComment parent = createArticleComment(parentCommentId, "댓글");
-        ArticleCommentDto child = createArticleCommentDto(parentCommentId, "대댓글");
-        given(articleRepository.getReferenceById(child.articleId())).willReturn(createArticle());
-        given(userAccountRepository.getReferenceById(child.userAccountDto().userId())).willReturn(createUserAccount());
-        given(articleCommentRepository.getReferenceById(child.parentCommentId())).willReturn(parent);
+        String oldContent = "content";
+        String updatedContent = "댓글";
+        ArticleComment articleComment = createArticleComment(oldContent);
+        ArticleCommentDto dto = createArticleCommentDto(updatedContent);
+        given(articleCommentRepository.getReferenceById(dto.id())).willReturn(articleComment);
 
         // When
-        sut.saveArticleComment(child);
+        sut.updateArticleComment(dto);
 
         // Then
-        assertThat(child.parentCommentId()).isNotNull();
-        then(articleRepository).should().getReferenceById(child.articleId());
-        then(userAccountRepository).should().getReferenceById(child.userAccountDto().userId());
-        then(articleCommentRepository).should().getReferenceById(child.parentCommentId());
-        then(articleCommentRepository).should(never()).save(any(ArticleComment.class));
+        assertThat(articleComment.getContent())
+                .isNotEqualTo(oldContent)
+                .isEqualTo(updatedContent);
+        then(articleCommentRepository).should().getReferenceById(dto.id());
+    }
+
+    @DisplayName("없는 댓글 정보를 수정하려고 하면, 경고 로그를 찍고 아무 것도 안 한다.")
+    @Test
+    void givenNonexistentArticleComment_whenUpdatingArticleComment_thenLogsWarningAndDoesNothing() {
+        // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleCommentRepository.getReferenceById(dto.id())).willThrow(EntityNotFoundException.class);
+
+        // When
+        sut.updateArticleComment(dto);
+
+        // Then
+        then(articleCommentRepository).should().getReferenceById(dto.id());
     }
 
     @DisplayName("댓글 ID를 입력하면, 댓글을 삭제한다.")
@@ -129,31 +131,21 @@ class ArticleCommentServiceTest {
     void givenArticleCommentId_whenDeletingArticleComment_thenDeletesArticleComment() {
         // Given
         Long articleCommentId = 1L;
-        String userId = "uno";
-        willDoNothing().given(articleCommentRepository).deleteByIdAndUserAccount_UserId(articleCommentId, userId);
+        willDoNothing().given(articleCommentRepository).deleteById(articleCommentId);
 
         // When
-        sut.deleteArticleComment(articleCommentId, userId);
+        sut.deleteArticleComment(articleCommentId);
 
         // Then
-        then(articleCommentRepository).should().deleteByIdAndUserAccount_UserId(articleCommentId, userId);
+        then(articleCommentRepository).should().deleteById(articleCommentId);
     }
 
 
     private ArticleCommentDto createArticleCommentDto(String content) {
-        return createArticleCommentDto(null, content);
-    }
-
-    private ArticleCommentDto createArticleCommentDto(Long parentCommentId, String content) {
-        return createArticleCommentDto(1L, parentCommentId, content);
-    }
-
-    private ArticleCommentDto createArticleCommentDto(Long id, Long parentCommentId, String content) {
         return ArticleCommentDto.of(
-                id,
+                1L,
                 1L,
                 createUserAccountDto(),
-                parentCommentId,
                 content,
                 LocalDateTime.now(),
                 "uno",
@@ -176,15 +168,12 @@ class ArticleCommentServiceTest {
         );
     }
 
-    private ArticleComment createArticleComment(Long id, String content) {
-        ArticleComment articleComment = ArticleComment.of(
-                createArticle(),
+    private ArticleComment createArticleComment(String content) {
+        return ArticleComment.of(
+                Article.of(createUserAccount(), "title", "content", "hashtag"),
                 createUserAccount(),
                 content
         );
-        ReflectionTestUtils.setField(articleComment, "id", id);
-
-        return articleComment;
     }
 
     private UserAccount createUserAccount() {
@@ -198,19 +187,12 @@ class ArticleCommentServiceTest {
     }
 
     private Article createArticle() {
-        Article article = Article.of(
+        return Article.of(
                 createUserAccount(),
                 "title",
-                "content"
+                "content",
+                "#java"
         );
-        ReflectionTestUtils.setField(article, "id", 1L);
-        article.addHashtags(Set.of(createHashtag(article)));
-
-        return article;
-    }
-
-    private Hashtag createHashtag(Article article) {
-        return Hashtag.of("java");
     }
 
 }
