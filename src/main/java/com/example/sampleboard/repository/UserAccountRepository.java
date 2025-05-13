eccc509fd91b0da1416e16c41517a79c6267fc7d
jhchoi <jhchoi@atoncorp.com>
2025-05-13 20:45:45 +0900

    UserAccount api 노출 추가 + data rest projection 을 이용한 데이터 노출 필드 설정

--- src/main/java/com/example/sampleboard/repository/UserAccountRepository.java
+++ src/main/java/com/example/sampleboard/repository/UserAccountRepository.java
@@ -1,7 +1,10 @@
 package com.example.sampleboard.repository;
 
 import com.example.sampleboard.domain.UserAccount;
+import com.example.sampleboard.domain.projection.UserAccountProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = UserAccountProjection.class)
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
